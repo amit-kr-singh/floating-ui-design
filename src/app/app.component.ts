82380b764234aa80d0f0c3a2453f059c3825e444
amit-kr-singh <amit16.jsr@gmail.com>
2023-07-31 12:54:11 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'angular-tour-of-heroes';
+
+  
+  itemArray = [
+    "Item 1",
+    "Item 2",
+    "Item 3"
+  ];
+
+  onEnter(){
+    let newVal =  (document.getElementById("newValue") as HTMLInputElement);
+    this.itemArray.push(newVal.value);
+    console.log("event value",newVal.value);
+    newVal.value = "";
+  }
+
+  onSelect(value: any) {
+    let selectedVal = (document.getElementById("selectedItem") as HTMLInputElement);
+    selectedVal.value = value;
+    console.log("selected value",selectedVal.value )
+  }
 }
